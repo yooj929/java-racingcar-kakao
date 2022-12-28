ea77b0b4778a5c45f03f0d2eec79918863bc8a38
yooj929 <jang0929@naver.com>
2022-12-28 13:14:27 +0900

    refactor: validate 중괄호 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,8 +7,6 @@ public class InputView {
     private static final int MAXIMUM_LENGTH = 5;
     private static final Scanner scanner = new Scanner(System.in);
 
-    public InputView() {
-    }
     public String[] inputName() {
         String[] names = scanner.nextLine().split(",");
         for (String name : names) {
@@ -17,11 +15,13 @@ public class InputView {
         return names;
     }
 
-    public int inputRunCount(){
+    public int inputRunCount() {
         return scanner.nextInt();
     }
+
     private void validate(String name) {
-        if(name.length() > MAXIMUM_LENGTH)
+        if (name.length() > MAXIMUM_LENGTH) {
             throw new RuntimeException();
+        }
     }
 }
